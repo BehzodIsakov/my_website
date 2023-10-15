288b7015c979610ce9be26ba488e1d6a2add9fbf
Bekzod <bekzodisakov18@gmail.com>
2023-10-15 15:46:36 +0500

    edit projects constant

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,15 +1,26 @@
 import { Project } from "./types";
 
-export const projects: [Project] = [
+export const projects: Project[] = [
   {
     title: "Personal collections app",
     description:
       "Collection app that lets create, update and delete collections of different items. There're 3 types of users. Anauthenticated users can only view collections. Authenticated users can control their collection while admin users can authorize other users, such as enabling/disabling other admins and blocking/unblocking users. The app also responds to different viewport sizes.",
-    imgSrc: "/assets/screenshot-store.png",
+    imgSrc: "/assets/screenshot-collection.png",
     links: {
       production: "https://personal-collections-app.netlify.app/",
       srcCode: "https://github.com/BekzodIsakov/personal-collections-app",
       demo: "",
+    },
+  },
+  {
+    title: "E-store",
+    description:
+      "This SPA has been bootstrapped with CRA and styled with Tailwind CSS. It contains a list of products of different categories which can be edited, deleted, created and filtered. The app also features pagination and a wildcard search.",
+    imgSrc: "/assets/screenshot-store.png",
+    links: {
+      production: "https://bekzodisakov.github.io/product-list/",
+      srcCode: "https://github.com/BekzodIsakov/product-list",
+      demo: "https://www.youtube.com/watch?v=lnrNaOMfSTA&ab_channel=user",
     },
   },
 ];
