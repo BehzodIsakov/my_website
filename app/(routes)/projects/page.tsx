383ce9a3b6f0523162e9b08002a1f38cd06f3e95
Bekzod <bekzodisakov18@gmail.com>
2023-10-15 15:42:26 +0500

    make Projects page responsive

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -5,15 +5,16 @@ export default function Page() {
   return (
     <>
       <h1 className='mb-8'>Projects</h1>
-      <div className='flex flex-wrap gap-x-[4%] gap-y-10 mb-3 pb-3 overflow-auto'>
+      <div className='flex flex-wrap gap-x-[4%] gap-y-12 mb-3 pb-3'>
         {projects.map((project, i) => (
-          <div key={i} className='basis-[48%]'>
+          <div key={i} className='sm:basis-[48%]'>
             <Card
               title={project.title}
               description={project.description}
               imgSrc={project.imgSrc}
               srcCode={project.links.srcCode}
               productionLink={project.links.production}
+              demoLink={project.links.demo}
             />
           </div>
         ))}
