4b32bf675885a50c925e5a2eb0be043f81056e3d
Bekzod <bekzodisakov18@gmail.com>
2023-10-15 13:48:53 +0500

    add new project

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -1,42 +1,22 @@
 import Card from "@/components/Card";
+import { projects } from "@/lib/constants";
 
 export default function Page() {
   return (
     <>
       <h1 className='mb-8'>Projects</h1>
       <div className='flex flex-wrap gap-x-[4%] gap-y-10 mb-3 pb-3 overflow-auto'>
-        <div className='sm:basis-[48%]'>
-          <Card
-            imgSrc='/assets/screenshot.png'
-            title='Project card'
-            description='Project description'
-            codeLink='https://github.com/BekzodIsakov'
-          />
-        </div>
-        <div className='sm:basis-[48%]'>
-          <Card
-            imgSrc='/assets/screenshot.png'
-            title='Project card'
-            description='Project description'
-            codeLink='https://github.com/BekzodIsakov'
-          />
-        </div>
-        <div className='sm:basis-[48%]'>
-          <Card
-            imgSrc='/assets/screenshot.png'
-            title='Project card'
-            description='Project description'
-            codeLink='https://github.com/BekzodIsakov'
-          />
-        </div>
-        <div className='sm:basis-[48%]'>
-          <Card
-            imgSrc='/assets/screenshot.png'
-            title='Project card'
-            description='Project description'
-            codeLink='https://github.com/BekzodIsakov'
-          />
-        </div>
+        {projects.map((project, i) => (
+          <div key={i} className='basis-[48%]'>
+            <Card
+              title={project.title}
+              description={project.description}
+              imgSrc={project.imgSrc}
+              srcCode={project.links.srcCode}
+              productionLink={project.links.production}
+            />
+          </div>
+        ))}
       </div>
     </>
   );
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,6 @@
 import Card from "./Card";
 import Link from "next/link";
+import { projects } from "@/lib/constants";
 
 export default function Projects() {
   return (
@@ -9,38 +10,17 @@ export default function Projects() {
         Some of the projects I&apos;ve built with a team and as a side project:
       </h3>
       <div className='flex sm:flex-wrap gap-x-[4%] gap-y-10 mb-3 pb-3 overflow-auto'>
-        <div className='basis-[48%]'>
-          <Card
-            imgSrc='/assets/screenshot.png'
-            title='Project card'
-            description='Project description'
-            codeLink='https://github.com/BekzodIsakov'
-          />
-        </div>
-        <div className='basis-[48%]'>
-          <Card
-            imgSrc='/assets/screenshot.png'
-            title='Project card'
-            description='Project description'
-            codeLink='https://github.com/BekzodIsakov'
-          />
-        </div>
-        <div className='basis-[48%]'>
-          <Card
-            imgSrc='/assets/screenshot.png'
-            title='Project card'
-            description='Project description'
-            codeLink='https://github.com/BekzodIsakov'
-          />
-        </div>
-        <div className='basis-[48%]'>
-          <Card
-            imgSrc='/assets/screenshot.png'
-            title='Project card'
-            description='Project description'
-            codeLink='https://github.com/BekzodIsakov'
-          />
-        </div>
+        {projects.map((project, i) => (
+          <div key={i} className='basis-[48%]'>
+            <Card
+              title={project.title}
+              description={project.description}
+              imgSrc={project.imgSrc}
+              srcCode={project.links.srcCode}
+              productionLink={project.links.production}
+            />
+          </div>
+        ))}
       </div>
       <Link href='/projects' className='link-outline'>
         View all
