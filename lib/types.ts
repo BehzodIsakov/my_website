ff740c3ce36cf01a0cca4ba82fbe4d1c0fd989c9
Bekzod <bekzodisakov18@gmail.com>
2023-10-15 13:47:45 +0500

    add Project interface

--- lib/types.ts
+++ lib/types.ts
@@ -4,3 +4,14 @@ export interface NavLink {
   name: string;
   href: string;
 }
+
+export interface Project {
+  title: string;
+  description: string;
+  imgSrc: string;
+  links: {
+    production: string;
+    srcCode: string;
+    demo?: string;
+  };
+}
