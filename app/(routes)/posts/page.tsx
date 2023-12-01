fe85185e58eefb2fd7220d901e07a80a9240a2f3
Bekzod <bekzodisakov18@gmail.com>
2023-12-01 20:19:55 +0500

    change Cpmming soon text color

--- app/(routes)/posts/page.tsx
+++ app/(routes)/posts/page.tsx
@@ -5,8 +5,8 @@ export default function Page() {
     <div>
       <h1>Posts</h1>
       <div className='mt-[20%] flex justify-center'>
-        <h2 className='w-max block text-center text-gradient'>
-          Coming soon! 🚂{" "}
+        <h2 className='w-max block text-center'>
+          <span className='text-gradient'>Coming soon!</span> 🚂
         </h2>
       </div>
     </div>
