274480bc382711cfa50c1644d5709608115e6869
Bekzod <bekzodisakov18@gmail.com>
2023-12-01 20:14:40 +0500

    Change Badge border color

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Badge({ children }: { children: React.ReactNode }) {
   return (
-    <div className='px-2 border border-neutral-200 dark:border-neutral-400 rounded-md bg-primary dark:bg-primary-dark'>
+    <div className='px-2 border border-neutral-200 dark:border-gray-500 rounded-md bg-primary dark:bg-primary-dark'>
       {children}
     </div>
   );
