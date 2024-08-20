1ba54930423e7eca38758f6ad3cb2763af27d8ea
Bekzod <bekzodisakov18@gmail.com>
2024-08-20 09:55:51 +0500

    Contact page

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -1,7 +1,12 @@
 export default function page() {
   return (
     <div>
-      <h1>Contact page</h1>
+      <h1>Contact</h1>
+      <div className='mt-[20%] flex justify-center'>
+        <h2 className='w-max block text-center'>
+          <span className='text-gradient'>Coming soon!</span> 🚂
+        </h2>
+      </div>
     </div>
   );
 }
