a7582a3cb17d50dda598d3a176e47f9334b2a65f
BekzodIsakov <bekzodisakov18@gmail.com>
2024-12-31 19:57:39 +0500

    re-write with fewer classes

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -1,12 +1,12 @@
-export default function page() {
+export default function ContactPage() {
   return (
-    <div>
+    <>
       <h1>Contact</h1>
-      <div className='mt-[20%] flex justify-center'>
-        <h2 className='w-max block text-center'>
+      <div className='mt-[20%]'>
+        <h2 className="w-full text-center">
           <span className='text-gradient'>Coming soon!</span> 🚂
         </h2>
       </div>
-    </div>
+    </>
   );
 }
--- app/(routes)/posts/page.tsx
+++ app/(routes)/posts/page.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-
-export default function Page() {
+export default function PostPage() {
   return (
-    <div>
+    <>
       <h1>Posts</h1>
-      <div className='mt-[20%] flex justify-center'>
-        <h2 className='w-max block text-center'>
+      <div className='mt-[20%]'>
+        <h2 className="w-full text-center">
           <span className='text-gradient'>Coming soon!</span> 🚂
         </h2>
       </div>
-    </div>
+    </>
   );
 }
